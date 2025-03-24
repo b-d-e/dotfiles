ddc7b8356d4c90b0d617411601b95d1f41cff6a7
Benjamin E <be@robots.ox.ac.uk>
2025-03-24 14:54:25 +0000

    tmux bits, zsh file

--- symlinks.sh
+++ symlinks.sh
@@ -1,3 +1,5 @@
 ln -s -f  ~/.dotfiles/git/.gitconfig ~/.gitconfig
 ln -s -f ~/.dotfiles/homebrew/Brewfile ~/Brewfile
 ln -s -f ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -s -f ~/.dotfiles/zsh/.zshrc ~/.zshrc
+ln -s -f ~/.dotfiles/zsh/.zshenv ~/.zshenv
